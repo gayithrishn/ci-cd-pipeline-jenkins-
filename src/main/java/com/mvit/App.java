0ef37a95a84a2d0bce1966604e051f68c0c0765f
gayithri <sonugowda6712gmail.com>
2026-03-28 12:25:05 +0530

    add some features

--- src/main/java/com/mvit/App.java
+++ src/main/java/com/mvit/App.java
@@ -10,5 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("jenkins is a CI/CD tool");
+
+        System.out.println("Docker it is also a main tool used in devops .. it is used for containiration");
     }
 }
