7f94b233af6fab1831f811e8a5884075e5888c2f
sonika <sonugowda6712gmail.com>
2026-03-28 12:14:08 +0530

    first commit

---
+++ src/main/java/com/mvit/App.java
@@ -0,0 +1,14 @@
+package com.mvit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("jenkins is a CI/CD tool");
+    }
+}
